35b7825969fe3eb32d4f179492b3977e7faa4d36
Code-Egg <eleu@litespeedtech.com>
2020-01-08 20:04:10 -0500

    appinstall fix

--- bin/setPassword.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-docker-compose exec litespeed su -s /bin/bash lsadm -c 'echo "admin:$(/usr/local/lsws/admin/fcgi-bin/admin_php* -q /usr/local/lsws/admin/misc/htpasswd.php '${1}')" > /usr/local/lsws/admin/conf/htpasswd';
-          
