91df8b2fa7ba64d63e8b046bdd210e2f6e42668e
Code-Egg <eric780217@gmail.com>
2025-10-24 08:19:44 +0800

    modified:   .travis/verify.sh

--- .travis/verify.sh
+++ .travis/verify.sh
@@ -43,14 +43,7 @@ verify_phpadmin(){
     else
         echo '[X]  http://localhost:8080/'
         exit 1
-    fi      
-    curl -sIk https://localhost:8443/ | grep -i phpMyAdmin
-    if [ ${?} = 0 ]; then
-        echo '[O]  http://localhost:8443/' 
-    else
-        echo '[X]  http://localhost:8443/'
-        exit 1
-    fi      
+    fi     
 }
 
 verify_add_vh_wp(){
