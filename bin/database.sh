3264a18b2770f72b53d067359137a63cab6305ad
stealthinnovative <stealthinnovative@gmail.com>
2026-01-14 10:05:02 -1000

    Update database.sh - mariadb standardization
    Update database.sh - mariadb standardization

--- bin/database.sh
+++ bin/database.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-source .env
 
 DOMAIN=''
 SQL_DB=''
@@ -83,7 +82,7 @@ EOT
 }
 
 check_db_access(){
-    docker compose exec -T mariadb su -c "mariadb -uroot --password=${MYSQL_ROOT_PASSWORD} -e 'status'" >/dev/null 2>&1
+    docker compose exec -T mariadb su -c "mariadb -uroot --password=${MARIADB_ROOT_PASSWORD} -e 'status'" >/dev/null 2>&1
     if [ ${?} != 0 ]; then
         echo '[X] DB access failed, please check!'
         exit 1
@@ -91,7 +90,7 @@ check_db_access(){
 }
 
 check_db_exist(){
-    docker compose exec -T mariadb su -c "test -e /var/lib/mysql/${1}"
+    docker compose exec -T mariadb su -c "test -e /var/lib/mariadb/${1}"
     if [ ${?} = 0 ]; then
         echo "Database ${1} already exist, skip DB creation!"
         exit 0    
@@ -99,7 +98,7 @@ check_db_exist(){
 }
 
 check_db_not_exist(){
-    docker compose exec -T mariadb su -c "test -e /var/lib/mysql/${1}"
+    docker compose exec -T mariadb su -c "test -e /var/lib/mariadb/${1}"
     if [ ${?} != 0 ]; then
         echo "Database ${1} doesn't exist, skip DB deletion!"
         exit 0
@@ -107,7 +106,7 @@ check_db_not_exist(){
 }
 
 db_setup(){  
-    docker compose exec -T mariadb su -c 'mariadb -uroot --password=${MYSQL_ROOT_PASSWORD} \
+    docker compose exec -T mariadb su -c 'mariadb -uroot --password=${MARIADB_ROOT_PASSWORD} \
     -e "CREATE DATABASE '${SQL_DB}';" \
     -e "GRANT ALL PRIVILEGES ON '${SQL_DB}'.* TO '${SQL_USER}'@'${ANY}' IDENTIFIED BY '${SQL_PASS}';" \
     -e "FLUSH PRIVILEGES;"'
@@ -123,7 +122,7 @@ db_delete(){
         SQL_USER="${SQL_DB}"
     fi
     check_db_not_exist ${SQL_DB}
-    docker compose exec -T mariadb su -c 'mariadb -uroot --password=${MYSQL_ROOT_PASSWORD} \
+    docker compose exec -T mariadb su -c 'mariadb -uroot --password=${MARIADB_ROOT_PASSWORD} \
         -e "DROP DATABASE IF EXISTS '${SQL_DB}';" \
         -e "DROP USER IF EXISTS '${SQL_USER}'@'${ANY}';" \
         -e "FLUSH PRIVILEGES;"'
